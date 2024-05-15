da56ca7e3d14494427a1820f05d8a830aeeaf78f
will wade <willwade@gmail.com>
2024-05-15 14:30:45 +0100

    added 32 bit and 64 bith paths

--- basic_demo_register_voice.py
+++ basic_demo_register_voice.py
@@ -8,26 +8,34 @@ import json
 import logging
 import winreg
 
-def register_voice(engine, language, voiceid, name, gender, vendor):
-    # Create the registry key for the voice
-    key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, f"SOFTWARE\\Microsoft\\SPEECH\\Voices\\Tokens\\{voiceid}")
 
-    # Set the attributes of the voice
-    winreg.SetValue(key, "", winreg.REG_SZ, name)
-    winreg.SetValue(key, "Language", winreg.REG_SZ, language)
-    winreg.SetValue(key, "Gender", winreg.REG_SZ, gender)
-    winreg.SetValue(key, "Vendor", winreg.REG_SZ, vendor)
-    winreg.SetValue(key, "CLSID", winreg.REG_SZ, str(PythonTTSVoice._reg_clsid_))
+def register_voice(voiceid, name, language, gender, vendor):
+    # Paths for 64-bit and 32-bit applications
+    paths = [
+        f"SOFTWARE\\Microsoft\\SPEECH\\Voices\\Tokens\\{voiceid}",
+        f"SOFTWARE\\WOW6432Node\\Microsoft\\SPEECH\\Voices\\Tokens\\{voiceid}"
+    ]
 
-    # Create the registry key for the attributes
-    attr_key = winreg.CreateKey(key, "Attributes")
-    winreg.SetValue(attr_key, "Language", winreg.REG_SZ, language)
-    winreg.SetValue(attr_key, "Gender", winreg.REG_SZ, gender)
-    winreg.SetValue(attr_key, "Vendor", winreg.REG_SZ, vendor)
+    for path in paths:
+        # Create the registry key for the voice
+        key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, path)
 
-    # Close the registry keys
-    winreg.CloseKey(attr_key)
-    winreg.CloseKey(key)
+        # Set the attributes of the voice
+        winreg.SetValue(key, "", winreg.REG_SZ, name)
+        winreg.SetValue(key, "409", winreg.REG_SZ, language)
+        winreg.SetValue(key, "CLSID", winreg.REG_SZ, "{4DFFD59B-4DF3-4366-B053-DFF9BE002EFB}")
+
+        # Create the registry key for the attributes
+        attr_key = winreg.CreateKey(key, "Attributes")
+        winreg.SetValue(attr_key, "Language", winreg.REG_SZ, language)
+        winreg.SetValue(attr_key, "Gender", winreg.REG_SZ, gender)
+        winreg.SetValue(attr_key, "Vendor", winreg.REG_SZ, vendor)
+        winreg.SetValue(attr_key, "Name", winreg.REG_SZ, name)
+
+        # Close the registry keys
+        winreg.CloseKey(attr_key)
+        winreg.CloseKey(key)
+
 
 class PythonTTSVoice(win32com.client.CDispatch):
     _public_methods_ = ['Speak', 'GetOutputFormat']
